1b4cc6515764ead87f274d64016518f8bbdb92cb
Anas Najjar <anas.najjar@logient.com>
2023-05-12 16:56:55 +0100

    adding styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,146 +1,72 @@
 import { Box, Flex, Icon, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { NavArrowDown } from "iconoir-react";
-import ContexLogo from "../../assets/context-logo.png";
+import ContexLogo from "../../assets/contex-logo.svg";
+import {
+  leftBottomTextStyle,
+  leftContainerStyle,
+  leftUpperTextStyle,
+  rightTextStyle,
+} from "./styles";
 
 const Footer: React.FC = () => {
   return (
     <Flex justify="center" p="22px 10px" bg="blue.700" height="150px">
       <Flex width="70%">
         {/* LEFT SIDE */}
-        <Flex
-          width="60%"
-          flexWrap="wrap"
-          direction="column"
-          justify="space-between"
-        >
+        <Flex sx={leftContainerStyle} direction="column">
           <Flex justify="space-between">
-            <Box
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="12px"
-              lineHeight="22px"
-              cursor="pointer"
-            >
+            <Box sx={leftUpperTextStyle}>
               <Text>Home</Text>
             </Box>
-            <Box
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="12px"
-              lineHeight="22px"
-              cursor="pointer"
-            >
+            <Box sx={leftUpperTextStyle}>
               <Text>Publications</Text>
             </Box>
-            <Flex
-              align="center"
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="12px"
-              lineHeight="22px"
-              cursor="pointer"
-            >
+            <Flex align="center" sx={leftUpperTextStyle}>
               <Text pr={1}>Rankings</Text>
               <Icon as={NavArrowDown} />
             </Flex>
-            <Flex
-              align="center"
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="12px"
-              lineHeight="22px"
-              cursor="pointer"
-            >
+            <Flex align="center" sx={leftUpperTextStyle}>
               <Text pr={1}>Analysis</Text>
               <Icon as={NavArrowDown} />
             </Flex>
-            <Flex
-              align="center"
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="12px"
-              lineHeight="22px"
-              cursor="pointer"
-            >
+            <Flex align="center" sx={leftUpperTextStyle}>
               <Text pr={1}>Management</Text>
               <Icon as={NavArrowDown} />
             </Flex>
-            <Flex
-              align="center"
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="12px"
-              lineHeight="22px"
-              cursor="pointer"
-            >
+            <Flex align="center" sx={leftUpperTextStyle}>
               <Text pr={1}>Data export</Text>
               <Icon as={NavArrowDown} />
             </Flex>
-            <Box
-              alignItems="center"
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="12px"
-              lineHeight="22px"
-              cursor="pointer"
-            >
+            <Box sx={leftUpperTextStyle}>
               <Text>Contact us</Text>
             </Box>
           </Flex>
           <Flex justify="space-between" position="relative" left="-50px">
-            <Text
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="10px"
-              lineHeight="22px"
-              textTransform="uppercase"
-            >
+            <Text sx={leftBottomTextStyle}>
               Copyright © 2022 Contex Group Inc.
             </Text>
-            <Text
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="10px"
-              lineHeight="22px"
-              textTransform="uppercase"
-              cursor="pointer"
-            >
+            <Text sx={leftBottomTextStyle} cursor="pointer">
               Terms & conditions
             </Text>
-            <Text
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="10px"
-              lineHeight="22px"
-              textTransform="uppercase"
-              cursor="pointer"
-            >
+            <Text sx={leftBottomTextStyle} cursor="pointer">
               Privacy
             </Text>
-            <Text
-              color="#FFFFFF"
-              fontWeight={700}
-              fontSize="10px"
-              lineHeight="22px"
-              textTransform="uppercase"
-              cursor="pointer"
-            >
+            <Text sx={leftBottomTextStyle} cursor="pointer">
               FAQ
             </Text>
           </Flex>
         </Flex>
         {/* RIGHT SIDE */}
         <Flex flexGrow={1} direction="column" align="flex-end">
-          <Image pb={4} width="164.76px" height="40px" src={ContexLogo} />
-          <Text
-            color="#FFFFFF"
-            fontWeight={700}
-            fontSize="10px"
-            lineHeight="16px"
-            textAlign="right"
-            textTransform="uppercase"
-          >
+          <Image
+            pb={4}
+            width="150px"
+            height="45px"
+            src={ContexLogo}
+            alt="contex logo"
+          />
+          <Text sx={rightTextStyle}>
             Contex Group Inc.
             <br /> 1100 René-Lévesque Blivd W.
             <br /> Montréal, QC H3B 4X9
