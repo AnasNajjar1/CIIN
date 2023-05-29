ad8d43afb5b412eb1d452539c822ce8176285fdf
Anas Najjar <anas.najjar@logient.com>
2023-05-29 16:54:50 +0100

    adding slider

--- src/components/HomePageThirdSection/index.tsx
+++ src/components/HomePageThirdSection/index.tsx
@@ -1,14 +1,38 @@
 import { Flex, Text, Icon, Box, Image } from "@chakra-ui/react";
 import { ArrowLeft, ArrowRight } from "iconoir-react";
 import React from "react";
+import Slider from "react-slick";
 import AsiIcon from "../../assets/asi_icon.svg";
 import CenturionIcon from "../../assets/centurion_icon.svg";
 import PhnIcon from "../../assets/phn_icon.svg";
 import FcIcon from "../../assets/fc_icon.svg";
 import BrIcon from "../../assets/br_icon.svg";
 import { iconStyle, textStyle } from "./styles";
+import "./styles.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HomePageThirdSection: React.FC = () => {
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    centerMode: true,
+    centerPadding: "5px",
+    slidesToShow: 4,
+    prevArrow: (
+      <Icon sx={iconStyle} as={ArrowLeft} _hover={{ color: "blue.500" }} />
+    ),
+    nextArrow: (
+      <Icon
+        sx={iconStyle}
+        as={ArrowRight}
+        _hover={{ color: "blue.500" }}
+        ml="15px"
+      />
+    ),
+  };
+
   return (
     <>
       <Flex h="267.87px" bg="gray.50" justify="center" p="50px">
@@ -21,14 +45,7 @@ const HomePageThirdSection: React.FC = () => {
           <Text sx={textStyle} pr={4}>
             Our clients
           </Text>
-          <Flex
-            justify="space-between"
-            align="center"
-            width="100%"
-            pr={10}
-            pl={2}
-          >
-            <Icon sx={iconStyle} as={ArrowLeft} />
+          <Slider {...settings} className="slider">
             <Box>
               <Image src={AsiIcon} alt="ASI icon" width="150px" />
             </Box>
@@ -39,13 +56,12 @@ const HomePageThirdSection: React.FC = () => {
               <Image src={PhnIcon} alt="Philips icon" width="150px" />
             </Box>
             <Box>
-              <Image src={FcIcon} alt="Fiera icon" width="100px" pb={5} />
+              <Image src={FcIcon} alt="Fiera icon" width="100px" />
             </Box>
             <Box>
               <Image src={BrIcon} alt="Black Rock icon" width="150px" />
             </Box>
-            <Icon sx={iconStyle} as={ArrowRight} />
-          </Flex>
+          </Slider>
         </Flex>
       </Flex>
     </>
